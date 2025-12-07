7573ef877f40b3bd718ffac08c91bedc115ed28d
isanogo97 <ibrahim.sanogo63@gmail.com>
2025-12-07 14:22:53 +0100

    Update manifest and layout with new icon paths

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -31,7 +31,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="fr" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/icon-32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/icon-16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/icon-180.png" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
       </head>
